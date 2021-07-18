4456827665ef01929bccc634bacdfbeb12346fb0
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-17 17:18:49 -0700

    Add `cron` job.

--- update_issues.sh
+++ update_issues.sh
@@ -3,7 +3,7 @@ cd ./tests/$1
 fail_msg="Tests failed; see \"Run tests\" step for more information."
 
 issue_title="\`$1\` failure"
-if curl --fail https://api.github.com/repos/$2/issues?state=open | jq "map(select(.title == \"$issue_title\"))" | jq '.[0]' -e | jq '.number' -e &> .number
+if curl --fail https://api.github.com/repos/$3/issues?state=open | jq "map(select(.title == \"$issue_title\"))" | jq '.[0]' -e | jq '.number' -e &> .number
 then
   number=$(cat .number)
   if test -f ".fail"
@@ -17,16 +17,16 @@ then
     if [ "$6" = "pull_request" ]; then echo "Fix implemented; not closing issue #$number for a PR (will be closed on merge)..."; exit 0; fi
     echo "Test passed; closing issue #$number..."
     curl --request POST \
-    --url https://api.github.com/repos/$2/issues/$number/comments \
-    --header "authorization: Bearer $3" \
+    --url https://api.github.com/repos/$3/issues/$number/comments \
+    --header "authorization: Bearer $2" \
     --header 'content-type: application/json' \
     --data "{ \
-      \"body\": \"Resolved @ $5; see workflow [$4](https://github.com/$2/actions/runs/$4)\"\
+      \"body\": \"Resolved @ $5; see workflow [$4](https://github.com/$3/actions/runs/$4)\"\
       }" \
     --fail &> /dev/null
     curl --request PATCH \
-    --url https://api.github.com/repos/$2/issues/$number \
-    --header "authorization: Bearer $3" \
+    --url https://api.github.com/repos/$3/issues/$number \
+    --header "authorization: Bearer $2" \
     --header 'content-type: application/json' \
     --data "{ \
       \"state\": \"closed\"\
@@ -40,12 +40,12 @@ else
     # do not allow PRs to introduce failures
     if [ "$6" = "pull_request" ]; then echo "$fail_msg Not creating new issue for a PR..."; exit 1; fi
     curl --request POST \
-    --url https://api.github.com/repos/$2/issues \
-    --header "authorization: Bearer $3" \
+    --url https://api.github.com/repos/$3/issues \
+    --header "authorization: Bearer $2" \
     --header 'content-type: application/json' \
     --data "{ \
       \"title\": \"$issue_title\",\
-      \"body\": \"\`$1\` failure @ $5; see workflow [$4](https://github.com/$2/actions/runs/$4) \n\n $(cat issue_body)\"\
+      \"body\": \"\`$1\` failure @ $5; see workflow [$4](https://github.com/$3/actions/runs/$4) \n\n $(cat issue_body)\"\
       }" \
     --fail &> /dev/null
     echo "$fail_msg"
