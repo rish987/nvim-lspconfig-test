0b2052bcbe2d8dd51f465aa4e4368cde8c3002d3
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-19 16:42:13 -0700

    Pre-build `leanls` test fixtures.

--- tests/leanls/setup.sh
+++ tests/leanls/setup.sh
@@ -1,3 +1,12 @@
+set -e 
+
 # set up lean language server
 curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh -s -- --default-toolchain "leanprover/lean4:nightly-2021-07-09" -y
 echo "$HOME/.elan/bin/" >> $GITHUB_PATH
+PATH="$PATH:$HOME/.elan/bin/"
+
+# pre-build fixtures
+( cd fixtures/example-project-1 && leanpkg configure && leanpkg build; )
+
+# pre-build fixtures
+( cd fixtures/example-project-2 && leanpkg configure && leanpkg build; )
