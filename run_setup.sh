e5322f74429f289d64b18d64561e047030f4cd6d
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-19 18:51:32 -0500

    Pre-build `leanls` test fixtures. (#21)

--- run_setup.sh
+++ run_setup.sh
@@ -2,4 +2,6 @@ mkdir packpath
 git clone --depth 1 https://github.com/neovim/nvim-lspconfig packpath/nvim-lspconfig
 git clone --depth 1 https://github.com/nvim-lua/plenary.nvim packpath/plenary.nvim
 
-./tests/$1/setup.sh 
+cd ./tests/$1
+
+./setup.sh 
