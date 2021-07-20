f5301874769531d159366bba0a3ec67be96e144d
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-19 18:19:33 -0700

    Use `Makefile`-driven approach (rather than bash).

---
+++ Makefile
@@ -0,0 +1,16 @@
+.PHONY: setup test
+
+check_test_var = if [ -z ${TEST} ]; then echo "must specify \$$TEST"; exit 1; fi
+
+packpath: 
+	mkdir packpath
+	git clone --depth 1 https://github.com/neovim/nvim-lspconfig packpath/nvim-lspconfig
+	git clone --depth 1 https://github.com/nvim-lua/plenary.nvim packpath/plenary.nvim
+
+setup: packpath
+	@${check_test_var}
+	cd ./tests/${TEST} && ./setup.sh 
+
+test: packpath
+	@${check_test_var}
+	( export NVIM_TEST_PATH=$(realpath .); cd ./tests/${TEST} && ./test.sh; )
--- tests/leanls/setup.sh
+++ tests/leanls/setup.sh
@@ -1,9 +1,15 @@
 set -e 
 
 # set up lean language server
-curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh -s -- --default-toolchain "leanprover/lean4:nightly-2021-07-09" -y
-echo "$HOME/.elan/bin/" >> $GITHUB_PATH
-PATH="$PATH:$HOME/.elan/bin/"
+if ! which elan > /dev/null; then
+  curl https://raw.githubusercontent.com/Kha/elan/master/elan-init.sh -sSf | sh -s -- --default-toolchain "leanprover/lean4:nightly-2021-07-09" -y
+  if [ ! -z ${GITHUB_PATH+x} ]; then 
+    echo "$HOME/.elan/bin/" >> $GITHUB_PATH; 
+  else
+    echo 'PATH="$HOME/.elan/bin:$PATH"' >> $HOME/.profile
+  fi
+  PATH="$HOME/.elan/bin/:$PATH"
+fi
 
 # pre-build fixtures
 ( cd fixtures/example-project-1 && leanpkg configure && leanpkg build; )
