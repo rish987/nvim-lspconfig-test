5839ed3aed5da3ae75872132cc62b98a254afdb3
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-20 09:16:41 -0700

    Use matrix for configs.

--- update_issues.sh
+++ update_issues.sh
@@ -1,32 +1,40 @@
-cd ./tests/$1
+server=$1
+key=$2
+repo=$3
+workflow=$4
+commit=$5
+trigger=$6
+
+cd ./tests/$server
 
 fail_msg="Tests failed; see \"Run tests\" step for more information."
 
-issue_title="\`$1\` failure"
-if curl -s --fail https://api.github.com/repos/$3/issues?state=open | jq "map(select(.title == \"$issue_title\"))" | jq '.[0]' -e | jq '.number' -e &> .number
+issue_title="\`$server\` failure"
+
+if curl -s --fail https://api.github.com/repos/$repo/issues?state=open | jq "map(select(.title == \"$issue_title\"))" | jq '.[0]' -e | jq '.number' -e &> .number
 then
   number=$(cat .number)
   if test -f ".fail"
   then
     # ignore existing errors in PRs
-    if [ "$6" = "pull_request" ]; then echo "$fail_msg Ignoring existing issue #$number for this PR..."; exit 0; fi
+    if [ "$trigger" = "pull_request" ]; then echo "$fail_msg Ignoring existing issue #$number for this PR..."; exit 0; fi
     echo "Existing issue found: #$number."
     echo "$fail_msg"
     exit 1
   else
-    if [ "$6" = "pull_request" ]; then echo "Fix implemented; not closing issue #$number for a PR (will be closed on merge)..."; exit 0; fi
+    if [ "$trigger" = "pull_request" ]; then echo "Fix implemented; not closing issue #$number for a PR (will be closed on merge)..."; exit 0; fi
     echo "Test passed; closing issue #$number..."
     curl --request POST \
-    --url https://api.github.com/repos/$3/issues/$number/comments \
-    --header "authorization: Bearer $2" \
+    --url https://api.github.com/repos/$repo/issues/$number/comments \
+    --header "authorization: Bearer $key" \
     --header 'content-type: application/json' \
     --data "{ \
-      \"body\": \"Resolved @ $5; see workflow [$4](https://github.com/$3/actions/runs/$4)\"\
+      \"body\": \"Resolved @ $commit; see workflow [$workflow](https://github.com/$repo/actions/runs/$workflow)\"\
       }" \
     --fail &> /dev/null
     curl --request PATCH \
-    --url https://api.github.com/repos/$3/issues/$number \
-    --header "authorization: Bearer $2" \
+    --url https://api.github.com/repos/$repo/issues/$number \
+    --header "authorization: Bearer $key" \
     --header 'content-type: application/json' \
     --data "{ \
       \"state\": \"closed\"\
@@ -38,14 +46,14 @@ else
   if test -f ".fail"
   then
     # do not allow PRs to introduce failures
-    if [ "$6" = "pull_request" ]; then echo "$fail_msg Not creating new issue for a PR..."; exit 1; fi
+    if [ "$trigger" = "pull_request" ]; then echo "$fail_msg Not creating new issue for a PR..."; exit 1; fi
     curl --request POST \
-    --url https://api.github.com/repos/$3/issues \
-    --header "authorization: Bearer $2" \
+    --url https://api.github.com/repos/$repo/issues \
+    --header "authorization: Bearer $key" \
     --header 'content-type: application/json' \
     --data "{ \
       \"title\": \"$issue_title\",\
-      \"body\": \"\`$1\` failure @ $5; see workflow [$4](https://github.com/$3/actions/runs/$4) \n\n $(cat issue_body)\"\
+      \"body\": \"\`$server\` failure @ $commit; see workflow [$workflow](https://github.com/$repo/actions/runs/$workflow) \n\n $(cat issue_body)\"\
       }" \
     --fail &> /dev/null
     echo "$fail_msg"
