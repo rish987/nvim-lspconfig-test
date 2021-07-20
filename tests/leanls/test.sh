0a363622e727b900d6092d6a4502ca8cb8d9c8c0
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-19 18:55:53 -0700

    TEST: persist failure.

--- tests/leanls/test.sh
+++ tests/leanls/test.sh
@@ -1,3 +1,4 @@
 echo $NVIM_TEST_PATH
 nvim --headless --noplugin -u $NVIM_TEST_PATH/minimal_init.lua -c "PlenaryBustedDirectory tests/ { minimal_init = '$NVIM_TEST_PATH/minimal_init.lua' }"
 false
+false
