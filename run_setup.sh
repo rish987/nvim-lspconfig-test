b6e9ed1dec3ddbbf82fb45425c584b83e6cdda3e
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-19 20:21:04 -0500

    Use `Makefile`-driven approach (rather than bash).

--- run_setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir packpath
-git clone --depth 1 https://github.com/neovim/nvim-lspconfig packpath/nvim-lspconfig
-git clone --depth 1 https://github.com/nvim-lua/plenary.nvim packpath/plenary.nvim
-
-cd ./tests/$1
-
-./setup.sh 
