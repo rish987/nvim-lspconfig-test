0f7d80a2b2900c8f2144a42537e631487d403ae4
Rishikesh Vaishnav <rishhvaishnav@gmail.com>
2021-07-21 09:08:25 -0700

    TEST: Fix all.

--- tests/lean3ls/test.sh
+++ tests/lean3ls/test.sh
@@ -1,6 +1,3 @@
 set -e
 
 nvim --headless --noplugin -u $NVIM_TEST_PATH/minimal_init.lua -c "PlenaryBustedDirectory tests/ { minimal_init = '$NVIM_TEST_PATH/minimal_init.lua' }"
-
-false
-if [ $RUNNER_OS = "macOS" ]; then false; fi
--- tests/leanls/test.sh
+++ tests/leanls/test.sh
@@ -1,5 +1,3 @@
 set -e
 
 nvim --headless --noplugin -u $NVIM_TEST_PATH/minimal_init.lua -c "PlenaryBustedDirectory tests/ { minimal_init = '$NVIM_TEST_PATH/minimal_init.lua' }"
-
-if [ $RUNNER_OS = "macOS" ]; then false; fi
