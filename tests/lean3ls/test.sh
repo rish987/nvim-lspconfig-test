4a07998c906d08e8102b44253b80e1a121275307
William Boman <william@redwill.se>
2021-12-08 15:27:16 +0100

    shellcheck

--- tests/lean3ls/test.sh
+++ tests/lean3ls/test.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-nvim --headless --noplugin -u $NVIM_TEST_PATH/minimal_init.lua -c "PlenaryBustedDirectory tests/ { minimal_init = '$NVIM_TEST_PATH/minimal_init.lua' }"
+nvim --headless --noplugin -u "$NVIM_TEST_PATH/minimal_init.lua" -c "PlenaryBustedDirectory tests/ { minimal_init = '$NVIM_TEST_PATH/minimal_init.lua' }"
--- update_issues.sh
+++ update_issues.sh
@@ -10,18 +10,18 @@ workflow=$GITHUB_RUN_ID
 commit=$GITHUB_SHA
 trigger=$GITHUB_EVENT_NAME
 
-cd ./tests/$server
+cd "./tests/$server"
 
 fail_msg="Tests failed; see \"Run tests\" step for more information."
 
 issue_title="\`$server\` failure on \`$os\`"
 
-curl -sS --fail --header "authorization: Bearer $key" $GITHUB_API_URL/repos/$repo/issues?state=open > .issues
+curl -sS --fail --header "authorization: Bearer $key" "$GITHUB_API_URL/repos/$repo/issues?state=open" > .issues
 
 test -s .issues
 
 # existing issue
-if cat .issues | jq "map(select(.title == \"$issue_title\"))" | jq '.[0]' -e | jq '.number' -e > .number
+if jq "map(select(.title == \"$issue_title\"))" .issues | jq '.[0]' -e | jq '.number' -e > .number
 then
   number=$(cat .number)
   if test -f ".fail"
@@ -35,7 +35,7 @@ then
     if [ "$trigger" = "pull_request" ]; then echo "Fix implemented; not closing issue #$number for a PR (will be closed on merge)..."; exit 0; fi
     echo "Test passed; closing issue #$number..."
     curl --request POST \
-    --url $GITHUB_API_URL/repos/$repo/issues/$number/comments \
+    --url "$GITHUB_API_URL/repos/$repo/issues/$number/comments" \
     --header "authorization: Bearer $key" \
     --header 'content-type: application/json' \
     --data "{ \
@@ -43,7 +43,7 @@ then
       }" \
     --fail &> /dev/null
     curl --request PATCH \
-    --url $GITHUB_API_URL/repos/$repo/issues/$number \
+    --url "$GITHUB_API_URL/repos/$repo/issues/$number" \
     --header "authorization: Bearer $key" \
     --header 'content-type: application/json' \
     --data "{ \
@@ -60,7 +60,7 @@ then
   # do not allow PRs to introduce failures
   if [ "$trigger" = "pull_request" ]; then echo "$fail_msg Not creating new issue for a PR..."; exit 1; fi
   curl --request POST \
-  --url https://api.github.com/repos/$repo/issues \
+  --url "https://api.github.com/repos/$repo/issues" \
   --header "authorization: Bearer $key" \
   --header 'content-type: application/json' \
   --data "{ \
